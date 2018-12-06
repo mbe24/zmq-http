b8c6645a517d03e5ebc5804f2d770ee41242e6af
Mikael Beyene <mikael@beyene.org>
2018-12-06 14:12:45 +0100

    Revise code
    Rename variables
    Use ZMsg for HTTP response

--- src/main/java/org/beyene/zmq/Application.java
+++ src/main/java/org/beyene/zmq/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        String message = new StringJoiner("\r\n")
+        String httpResponse = new StringJoiner("\r\n")
                 .add("HTTP/1.1 200 OK")
                 .add("Content-Type: text/html")
                 .add("")
@@ -25,30 +25,29 @@ public class Application {
 
             System.out.println("Server is listening on http://localhost:8080");
             while (!Thread.currentThread().isInterrupted()) {
-                ZMsg msg = ZMsg.recvMsg(socket);
+                ZMsg request = ZMsg.recvMsg(socket);
 
-                if (msg.isEmpty() || msg.size() != 2)
+                if (request.isEmpty() || request.size() != 2)
                     continue;
 
-                ZFrame identityFrame = msg.poll();
-                ZFrame messageFrame = msg.poll();
+                ZFrame identityFrame = request.poll();
+                ZFrame messageFrame = request.poll();
 
-                String request = new String(messageFrame.getData(), ZMQ.CHARSET);
-                if (request.isEmpty())
+                String httpRequest = new String(messageFrame.getData(), ZMQ.CHARSET);
+                if (httpRequest.isEmpty())
                     continue;
 
                 System.out.println("ID:" + identityFrame.strhex());
-                System.out.println("REQUEST:" + System.lineSeparator() + request);
+                System.out.println("REQUEST:" + System.lineSeparator() + httpRequest);
 
-                // specify receiver by sending identity
-                socket.send(identityFrame.getData(), ZMQ.SNDMORE);
-                // send http response
-                socket.send(message.getBytes(ZMQ.CHARSET), ZMQ.SNDMORE);
+                ZMsg response = new ZMsg();
+                response.add(identityFrame.getData()); // specify receiver by sending identity
+                response.add(httpResponse.getBytes(ZMQ.CHARSET)); // send http response
 
-                // specify receiver by sending identity
-                socket.send(identityFrame.getData(), ZMQ.SNDMORE);
-                // close connection by sending empty message
-                socket.send(new byte[0]);
+                response.add(identityFrame.getData()); // specify receiver by sending identity
+                response.add(new byte[0]); // close connection by sending empty message
+
+                response.send(socket);
             }
 
         }
